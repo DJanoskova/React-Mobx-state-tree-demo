49f0e33b4dfe8b68c6f62465e6ffdef415430107
Dana Janoskova <dana.janoskova@accenture.com>
2019-04-02 22:16:53 +0200

    todos group together

--- src/store/models/TodoStore.js
+++ src/store/models/TodoStore.js
@@ -1,6 +1,21 @@
 import { types } from 'mobx-state-tree';
 
-import Todo from './Todo';
+const Todo = types
+  .model('Todo', {
+    name: types.string,
+    details: types.string,
+    is_done: false
+  })
+  .actions(self => ({
+    markDone() {
+      self.is_done = true;
+    }
+  }))
+  .views(self => ({
+    status() {
+      return self.is_done ? 'Done' : 'Not Done'
+    }
+  }));
 
 const TodoStore = types
   .model('Todo', {
